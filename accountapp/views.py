b34e6b42455bbb1801cf4cac6d4c346d138fee28
monkeyz <sangmin3285@gmail.com>
2021-07-08 12:58:43 +0900

    add get&post

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+def hello_world(req):
+    if req.method == 'POST':
+        return render(req, 'accountapp/hello_world.html',
+                      context={'tttt' : 'POST METHOD'})
+    else:
+        return render(req, 'accountapp/hello_world.html',
+                      context={'tttt': 'GET METHOD'})
