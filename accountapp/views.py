e32e45d449541c0643047c31ef75a29acfe74c7e
monkeyz <sangmin3285@gmail.com>
2021-07-12 11:14:56 +0900

    add input text

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def hello_world(req):
     if req.method == 'POST':
+
+        temp = req.POST.get('input_text')
+
+
         return render(req, 'accountapp/hello_world.html',
-                      context={'tttt' : 'POST METHOD'})
+                      context={'tttt' : temp})
     else:
         return render(req, 'accountapp/hello_world.html',
                       context={'tttt': 'GET METHOD'})
