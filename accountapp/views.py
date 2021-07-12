c39ce52f852a2b51083aa9fea6ec1c09678b8992
monkeyz <sangmin3285@gmail.com>
2021-07-12 12:13:54 +0900

    modified view.py to show all text

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,8 +12,13 @@ def hello_world(req):
         new_hello_world.text = temp
         new_hello_world.save()
 
+        new_hello_world_list = HelloWorld.objects.all()
+
+
         return render(req, 'accountapp/hello_world.html',
-                      context={'new_hello_world' : new_hello_world})
+                      context={'new_hello_world' : new_hello_world, 'new_hello_world_list' : new_hello_world_list})
     else:
+        new_hello_world_list = HelloWorld.objects.all()
+
         return render(req, 'accountapp/hello_world.html',
-                      context={'tttt': 'GET METHOD'})
+                      context={'new_hello_world_list': new_hello_world_list})
