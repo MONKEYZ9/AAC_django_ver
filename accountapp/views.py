60faa289adb9561e8c832e589992b7e44534ddfd
monkeyz <sangmin3285@gmail.com>
2021-07-12 11:36:06 +0900

    add sql properties & modified views.py

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
+
 def hello_world(req):
     if req.method == 'POST':
 
         temp = req.POST.get('input_text')
-
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
         return render(req, 'accountapp/hello_world.html',
-                      context={'tttt' : temp})
+                      context={'new_hello_world' : new_hello_world})
     else:
         return render(req, 'accountapp/hello_world.html',
                       context={'tttt': 'GET METHOD'})
