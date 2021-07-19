10eee25297c100d915a2dc414cf428cb877cac1e
monkeyz <sangmin3285@gmail.com>
2021-07-19 22:18:28 +0900

    update profile update

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
@@ -26,3 +26,11 @@ class ProfileCreateView(CreateView):
         # 저장하기
         temp_profile.save()
         return super().form_valid(form)
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello world')
+    template_name = 'profileapp/update.html'
